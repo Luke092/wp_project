386a2986e9687e1946f7cbf673c2f823a281b3dd
Michele92 <michelemasciale92@gmail.com>
2015-06-05 22:55:23 +0200

    Aggiunto index.php e relativo stile
    Aggiunta la pagina iniziale ancora priva di logica, ma perlomeno con un
    minimo di stile

--- src/index.php
+++ src/index.php
@@ -7,11 +7,15 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="ISO-8859-1">
-        <title></title>
+        <link rel = "stylesheet" type = "text/css" href = "./css/index_style.css">
+        <title>Autenticazione</title>
     </head>
     <body>
-        <?php
-        
-        ?>
+    <center>
+        <img id="header" src="./img/index_header.jpg"/><br/><br/>
+        <button id="login">Accedi</button>&nbsp;&nbsp;
+        <button id="register">Registrati</button>
+    </center>
+    <footer>&copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario</footer>
     </body>
 </html>
