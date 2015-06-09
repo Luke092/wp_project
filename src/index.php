ba8c184049fc50c547ebd15f79c749eeaaaafa78
Luca Bettinelli <luca.bettinelli92@gmail.com>
2015-06-09 16:46:33 +0200

    Modify index.php and it's style
    Add rss_aggregator.php as a template page

--- src/index.php
+++ src/index.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="ISO-8859-1">
         <link rel = "stylesheet" type = "text/css" href = "./css/index_style.css">
-        <title>Autenticazione</title>
+        <title>Home Page</title>
     </head>
     <body>
-    <center>
-        <img id="header" src="./img/index_header.jpg"/><br/><br/>
-        <button id="login">Accedi</button>&nbsp;&nbsp;
-        <button id="register">Registrati</button>
-    </center>
-    <footer>&copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario</footer>
+        <header>
+            <center>
+                <img id="header" src="./img/index_header.jpg"/>
+            </center>
+        </header>
+        <div class="content">
+            <center>
+                <button id="login">Accedi</button>&nbsp;&nbsp;
+                <button id="register">Registrati</button>
+            </center>
+        </div>
+        <footer>&copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario</footer>
     </body>
 </html>
---
+++ src/rss_aggregator.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="ISO-8859-1">
+        <link rel = "stylesheet" type = "text/css" href = "./css/index_style.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <header>
+            <!-- Top menu for account name, link to profile and manage button -->
+        </header>
+        <div class="content">
+            <div id="sidebar">
+                <p>paragraph</p>
+            </div>
+            <div id="page">
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+                <p>paragraph</p>
+            </div>
+        </div>    
+        <footer>&copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario</footer>
+    </body>
+</html>
