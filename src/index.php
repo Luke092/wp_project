8baea53a9582d20041a5258bef3afdd2a00fd89d
Michele92 <michelemasciale92@gmail.com>
2015-06-10 12:20:31 +0200

    Apportate modifiche generali

--- src/index.php
+++ src/index.php
@@ -2,21 +2,27 @@
 <html>
     <head>
         <meta charset="ISO-8859-1">
-        <link rel = "stylesheet" type = "text/css" href = "./css/style.css">
         <title>Home Page</title>
+        <link rel = "stylesheet" type = "text/css" href = "./css/generic.css">
+        <link rel = "stylesheet" type = "text/css" href = "./css/index.css">
+        <!--[if lt IE 7]>
+                <style type="text/css">
+                        #wrapper { height:100%; }
+                </style>
+        <![endif]-->
     </head>
     <body>
-        <div id="container">
-            <center>
-                <img src="./img/index_header.jpg"/>
-            </center>
-            <center>
+        <div id="wrapper">
+            <div id="header" align="center">
+                <img src="img/index_header.jpg"/>
+            </div>
+            <div id="content" align="center">
                 <button id="login">Accedi</button>&nbsp;&nbsp;
                 <button id="register">Registrati</button>
-            </center>
-        </div>
-        <div id="footer">
-            &copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario
+            </div>
+            <div id="footer">
+                &copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario
+            </div>
         </div>
     </body>
 </html>
--- src/rss_aggregator.php
+++ src/rss_aggregator.php
@@ -2,39 +2,49 @@
 <html>
     <head>
         <meta charset="ISO-8859-1">
-        <link rel = "stylesheet" type = "text/css" href = "./css/style.css">
+        <link rel="stylesheet" type="text/css" href="./css/generic.css">
+        <link rel="stylesheet" type="text/css" href="./css/home.css">
         <title>Home</title>
+        <!--[if lt IE 7]>
+		<style type="text/css">
+			#wrapper { height:100%; }
+		</style>
+	<![endif]-->
     </head>
     <body>
-        <header>
-            <!-- Top menu for account name, link to profile and manage button -->
-        </header>
-        <div class="content">
-            <div id="sidebar">
-                <p>paragraph</p>
+        <div id="wrapper">
+            <div id="header">
+                <!-- Top menu for account name, link to profile and manage button -->
             </div>
-            <div id="page">
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
-                <p>paragraph</p>
+            <div id="content">
+                <div id="sidebar">
+                    <p>paragraph</p>
+                </div>
+                <div id="page">
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                </div>
             </div>
-        </div>    
-        <footer>&copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario</footer>
+            <div id="footer">
+                &copy; 2015 Programmazione Web | Autori: Michele Masciale, Luca Bettinelli e Matteo Mario
+            </div>
+        </div>
     </body>
 </html>
