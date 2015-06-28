0a4f91b16ef575551dacc6a457db1ef57f3ca7d7
Michele92 <michelemasciale92@gmail.com>
2015-06-28 10:51:04 +0200

    Personalizzato lo stile del pop-up di login e registrazione

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@
                 </style>
         <![endif]-->
         
-        <script type="text/javascript" src="library/jquery-2.1.4.js"></script>
+        <script type="text/javascript" src="lib/jquery-2.1.4.js"></script>
         <script type="text/javascript" src="js/login.js"></script>
         
     </head>
@@ -26,42 +26,42 @@
                 <button id="register" value="#register-box">Registrati</button>
                 
                 <div id="login-box" class="login-popup">
-                    <a href="#" class="close"><img src="http://www.alessioatzeni.com/wp-content/tutorials/jquery/login-box-modal-dialog-window/close_pop.png" class="btn_close" title="Close Window" alt="Close" /></a>
+                    <a href="#" class="close"><img src="img/close_pop.png" class="btn_close" title="Chiudi" alt="Close" /></a>
                     <form method="post" class="signin" action="#">
                         <fieldset class="textbox">
                             <label class="username">
                                 <span>Email</span>
-                                <input id="email" name="username" value="" type="text" autocomplete="on" placeholder="Username">
+                                <input id="email" name="email" value="" type="text" autocomplete="on" placeholder="Email">
                             </label>
                             <label class="password">
                                 <span>Password</span>
                                 <input id="password" name="password" value="" type="password" placeholder="Password">
                             </label>
-                            <button class="submit button" type="button">Sign in</button>
-                            <p>
-                                <a class="forgot" href="#">Forgot your password?</a>
-                            </p>        
+                            <button class="submit button" type="button">Accedi</button>
+<!--                            <p>
+                                <a class="forgot" href="#">Hai dimenticato la tua password?</a>
+                            </p>        -->
                         </fieldset>
                     </form>
                 </div>
                 
                 <div id="register-box" class="login-popup">
-                    <a href="#" class="close"><img src="http://www.alessioatzeni.com/wp-content/tutorials/jquery/login-box-modal-dialog-window/close_pop.png" class="btn_close" title="Close Window" alt="Close" /></a>
+                    <a href="#" class="close"><img src="img/close_pop.png" class="btn_close" title="Chiudi" alt="Close" /></a>
                     <form method="post" class="signin" action="#">
                         <fieldset class="textbox">
                             <label class="username">
                                 <span>Email</span>
-                                <input id="email" name="username" value="" type="text" autocomplete="on" placeholder="Username">
+                                <input id="email" name="email" value="" type="text" autocomplete="on" placeholder="Email">
                             </label>
                             <label class="password">
                                 <span>Password</span>
                                 <input id="password" name="password" value="" type="password" placeholder="Password">
                             </label>
                             <label class="password">
-                                <span>Insert Password Again</span>
-                                <input id="re-password" name="password" value="" type="password" placeholder="Repeate Password">
+                                <span>Ripeti password</span>
+                                <input id="re-password" name="password" value="" type="password" placeholder="Ripeti password">
                             </label>
-                            <button class="submit button" type="button">Register</button>
+                            <button class="submit button" type="button">Registrati</button>
                         </fieldset>
                     </form>
                 </div>
